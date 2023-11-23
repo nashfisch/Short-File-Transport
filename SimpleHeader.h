9c32dec2e0e3d9303bade576b71d4d8292ac85a4
nashfisch <Nash.Fisch88@gmail.com>
2023-11-22 21:06:20 -0700

    Comments added

--- SimpleHeader.h
+++ SimpleHeader.h
@@ -2,76 +2,78 @@
 #define __SIMPLEHEADER_H
 
 #include <cstdint>
-// maximum size for the payload 
+//Max payload size
 #define DATA_SZ 1024
-// size of header
+//Header size
 #define HEADER_SZ 4
 
 
 struct simplepacket {
   uint8_t header[HEADER_SZ];
-  char data[DATA_SZ];  // payload
+  char data[DATA_SZ];  //payload
 };
 
-// class to be tested. Implements the demo header structure below.
-// exports routines to get and set the header parameters val (integer between 0 and 3) and payload_length (16 bit unsigned integer) for a buffer containing the packet
-//  0 1  2 7   8 15  16   32
-// +---+-----+-----+--------+
-// |Val| unu | unu |payload |
-// |   | sed | sed |length  |
-// +---+-----+-----+--------+
-// NOTE: this class does not allocate memory for the packet buffer. Memory for the buffer must be already allocated by the calling code.
 
 class SimpleHeader {
 private:
-  uint8_t * packet;  // address of the buffer
-  int size;          // size of buffer in bytes
+  uint8_t * packet;  //Buffer Address
+  int size;          //Size of buffer in bytes
 
-  // start index of the payload length field (private)
+  //Starting index of the payload
   const int PL=2;
 
-  // start index of val header field
+  //Sarting index of the val
   const int VL=0;
   
 public:
-  // default constructor, packet buffer address set to NULL
+  //Default constructor
   SimpleHeader();
 
-  // assigns an externally allocated buffer to the class; the set/get member functions will manage the headers fields for this buffer
-  // buf = the address of the buffer
-  // sz = the size in bytes of the buffer
+  //A buffer for the class. Setters and getters within the class will be used to modify its data.
   void setBuffer(void * buf, int sz);
   
-  // sets the value of the payload length
-  // val = length; if val > DATA_SZ, the value set is DATA_SZ
-  // if buffer address is NULL, does nothing.
+  //Sets the length of payload
   void setPayloadLength(unsigned int val);
 
-  // returns the length of the payload
-  // if buffer address is NULL, returns 0
+  //Returns the length of payload
   unsigned int getPayloadLength() const;
 
-  // returns the val field of the header. Must be between 0..3 since
-  // it is a 2 bit value
-  // if buffer address is NULL, returns 0
+  //Returns the type. Can be 0-3, as it is a 2 bit field.
   unsigned int getType() const;
 
-  // sets the value field of the header.
-  // If the val is not between 0..3, the value set is 0
-  // if buffer address is NULL, does nothing
+  //Sets the type. If it is not 0-3, it is set to 0.
   void setType(unsigned int val);
 
   unsigned int getTR() const;
   void setTR(unsigned int val);
+  //Returns the window
   unsigned int getWindow() const;
+
+  //Sets the window
   void setWindow(unsigned int val);
+
+  //Returns the sequence number.
   unsigned int getSeqNum() const;
+
+  //Sets the sequence number.
   void setSeqNum(unsigned int val);
+
+  //Returns the length of the whole packet.
   unsigned int getLength() const;
+
+  //Sets the length of the whole packet.
   void setLength(unsigned int val);
+
+  //Returns the CRC1 packet. CRC1 is done on the first 8 bits.
   unsigned int getCRC1() const;
+
+  //Sets the CRC1 of the packet. CRC1 applies to the whole packet.
   void setCRC1(unsigned int val);
+
+  //Sets the time stamp.
   void setTimeStamp(unsigned int val);
+
+  //Sets teh CRC2 of the packet. CRC2 applies to the just the payload.
   void setCRC2(unsigned int val, int payloadSize);
 
   // returns the size of the packet in bytes, including headers and data
@@ -80,14 +82,12 @@ public:
     return size;
   }
 
-  // returns pointer to the structure holding the thePacket, including the headers
-  // To be used with recvfrom or sendto
+  //Returns the packet itself. Used with sendto and rcvfrom.
   uint8_t* getBufferAddress() {
     return packet;
   }
 
-  // returns a pointer to the first byte of the payload from the packet buffer;
-  // returns NULL if the buffer is NULL
+  //Returns a pointer to the first byte of the payload.
   void * getPayloadAddress() {
     if (packet != nullptr && size > HEADER_SZ) {
       return packet+HEADER_SZ;
