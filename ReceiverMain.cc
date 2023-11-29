ac09e502ae300a2dccdb2fb35c4ed7ecaceedba1
Manraj Pannu <90409868+rnpannu@users.noreply.github.com>
2023-11-29 13:02:03 -0700

    Update ReceiverMain.cc

--- ReceiverMain.cc
+++ ReceiverMain.cc
@@ -15,36 +15,40 @@ int main(int argc, char* argv[]) {
     std::string fileName;
     std::string IPv;
     std::string receiverPort;
-    if (argc != 5) {
-        if (argc == 4 && argv[1] == "-f"){ //receiver -f filename port
+    if (argc != 5 && argc > 1) {
+        std::string fileCommand = argv[1];
+        if (argc == 4 && fileCommand == "-f"){ //receiver -f filename port
             fileName = argv[2];
             IPv = 4;
             receiverPort = argv[3];
-        } else if (argc == 3 && argv[1] == "-6") { // receiver -6 port
+        } else if (argc == 3 && fileCommand == "-6") { // receiver -6 port
             fileName = "Payload.txt";
-            IPv = argv[1];
+            IPv = fileCommand;
             receiverPort = argv[2];
         } else if (argc == 2) { // receiver port
             fileName = "Payload.txt";
             IPv = 4;
-            receiverPort = argv[1];
+            receiverPort = fileCommand;
         } else {
-            std::cerr << "Usage " << argv[0] << "[-6], receiver_port" << std::endl;
+            std::cerr << "Usage " << argv[0] << " [-6], receiver_port" << std::endl;
             return 1;
         }
-
-    } else if (argc == 5 && argv[1] == "-f" && argv[3] == "-6") {
-        fileName = argv[2];
-        IPv = argv[3];
-        receiverPort = argv[4];
-    } else if (argc == 4 && argv[1] == "-f") {
-        fileName = argv[2];
-        
-        IPv = 4;
-        receiverPort = argv[3];
-    } else {
-        std::cerr << "Usage " << argv[0] << "[-f filename], port" << std::endl;
-        return 1;
+    } 
+    else if (argc == 5){
+        std::string fileCommand = argv[1];
+        std::string IPvCommand = argv[3];
+        if (fileCommand == "-f" && IPvCommand == "-6"){
+            fileName = argv[2];
+            IPv = IPvCommand;
+            receiverPort = argv[4];
+        } else if (argc == 4 && fileCommand == "-f") {
+                fileName = argv[2];
+                IPv = 4;
+                receiverPort = argv[3];
+        } else {
+            std::cerr << "Usage " << argv[0] << " [-f filename], port" << std::endl;
+            return 1;
+        }
     }
 
 
